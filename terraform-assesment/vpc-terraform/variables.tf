4385793f3c88332aa512a9e004f19136d2be00a1
sidhujaswinder <44638773+sidhujaswinder@users.noreply.github.com>
2024-04-08 18:05:45 +0530

    Update variables.tf

--- terraform-assesment/vpc-terraform/variables.tf
+++ terraform-assesment/vpc-terraform/variables.tf
@@ -2,11 +2,6 @@ variable "region" {
   description = "aws resource region"
 }
 
-variable "vpc_cidr" {
-  description = "vpc cidr block"
-  default     = "10.1.0.0/16"
-}
-
 variable "vpc_name" {
   description = "vpc name"
 }
